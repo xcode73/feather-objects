f553e639bfe902930e87b8e039f49a1611fd3cf7
Tibor Bodecs <mail.tib@gmail.com>
2022-03-05 14:57:03 +0100

    test permissions

--- Tests/FeatherApiTests/FeatherApiTests.swift
+++ Tests/FeatherApiTests/FeatherApiTests.swift
@@ -10,7 +10,7 @@ import XCTest
 
 final class FeatherApiTests: XCTestCase {
 
-    func testFeatherPermissionEncode() async throws {
+    func testPermissionEncode() async throws {
         let permission = FeatherPermission("user.account.login")
         let encoder = JSONEncoder()
         let data = try encoder.encode(permission)
@@ -22,7 +22,7 @@ final class FeatherApiTests: XCTestCase {
         XCTAssertEqual(value, expectation, "Invalid action encode")
     }
     
-    func testFeatherPermissionDecode() async throws {
+    func testPermissionDecode() async throws {
         let value = "{\"namespace\":\"user\",\"context\":\"account\",\"action\":\"login\"}"
         let data = value.data(using: .utf8)!
         let decoder = JSONDecoder()
@@ -30,5 +30,37 @@ final class FeatherApiTests: XCTestCase {
                 
         let expectation = FeatherPermission("user.account.login")
         XCTAssertEqual(permission, expectation)
+    }
+    
+    func testUserPermissionLevels() async throws {
+        let guestUser = FeatherUser(id: .init(), level: .guest, roles: [])
+        let user = FeatherUser(id: .init(), level: .authenticated, roles: [])
+        let rootUser = FeatherUser(id: .init(), level: .root, roles: [])
+        let permission = FeatherPermission("user.account.login")
+        
+        XCTAssertFalse(guestUser.hasPermission(permission))
+        XCTAssertFalse(user.hasPermission(permission))
+        XCTAssertTrue(rootUser.hasPermission(permission))
+    }
+    
+    func testUserPermissionRoles() async throws {
+        let permission1 = FeatherPermission("user.account.login")
+        let permission2 = FeatherPermission("user.account.register")
+        let role = FeatherRole(key: "admins", permissions: [
+            permission1
+        ])
+        
+        let guestUser = FeatherUser(id: .init(), level: .guest, roles: [role])
+        let user = FeatherUser(id: .init(), level: .authenticated, roles: [role])
+        let rootUser = FeatherUser(id: .init(), level: .root, roles: [role])
+        
+        
+        XCTAssertTrue(guestUser.hasPermission(permission1))
+        XCTAssertTrue(user.hasPermission(permission1))
+        XCTAssertTrue(rootUser.hasPermission(permission1))
+        
+        XCTAssertFalse(guestUser.hasPermission(permission2))
+        XCTAssertFalse(user.hasPermission(permission2))
+        XCTAssertTrue(rootUser.hasPermission(permission2))
     }
 }
