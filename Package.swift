fd014ba456b6fb607cd6a67c9e0cc7879e0056e6
xcode73 <xcode.uln@gmail.com>
2024-02-20 15:24:16 +0400

    update to macos 12

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "feather-objects",
     platforms: [
-       .macOS(.v10_15),
+       .macOS(.v12),
     ],
     products: [
         .library(name: "FeatherObjects", targets: ["FeatherObjects"]),
